f418bb5bff25774807dc1ff7aaa01a938333946e
bharath <errorzz2004@gmail.com>
2025-07-13 00:30:41 +0530

    Add files via upload

---
+++ python day 2 ex/#Exaple 1 checking a condition.py
@@ -0,0 +1,11 @@
+#Exaple 1: checking a condition
+num= 0
+if num>10:
+    print("Postive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+      
+#OUTPUT
+#Negative number
---
+++ python day 2 ex/#Nested Condition.py
@@ -0,0 +1,10 @@
+#Nested Condition
+age=25
+if age>18:
+    if age<30:
+        print("Young adult")
+else:
+     print("adult")
+        
+#output 
+ #Young adult
